9f0a5bb3d821b1872763e3733970478c6c132456
ciaaaudiovisual <133988963+ciaaaudiovisual@users.noreply.github.com>
2025-07-25 23:40:36 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -4,6 +4,10 @@ from supabase import create_client
 @st.cache_resource
 def init_connection():
     """Inicializa e retorna o cliente de conexão com o Supabase."""
-    url = st.secrets["SUPABASE_URL"]
-    key = st.secrets["SUPABASE_KEY"]
-    return create_client(url, key)
+    try:
+        url = st.secrets["SUPABASE_URL"]
+        key = st.secrets["SUPABASE_KEY"]
+        return create_client(url, key)
+    except Exception as e:
+        st.error(f"Erro ao conectar com o Supabase. Verifique seus Secrets. Detalhes: {e}")
+        return None
